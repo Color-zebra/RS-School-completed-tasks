4c254d3578deb557f088c9a4026b6c2ef8507521
Romanenkov Dmitriy <215rd@mail.ru>
2023-06-07 19:17:54 +0300

    feat: migrate sources.ts file

---
+++ migration-to-typescript/src/components/view/sources/sources.ts
@@ -0,0 +1,31 @@
+import { Source } from '../../../interfaces/responceInterfaces';
+import './sources.css';
+
+class Sources {
+    draw(data: Source[]) {
+        const fragment = document.createDocumentFragment();
+        const sourceItemTemp: HTMLTemplateElement | null = document.querySelector('#sourceItemTemp');
+
+        data.forEach((item) => {
+            const sourceClone = sourceItemTemp?.content.cloneNode(true) as HTMLElement;
+
+            if (!sourceClone) return;
+
+            const sourceName = sourceClone.querySelector('.source__item-name');
+            if (sourceName) {
+                sourceName.textContent = item.name;
+            }
+
+            const sourceItem = sourceClone.querySelector('.source__item');
+            if (sourceItem) {
+                sourceItem.setAttribute('data-source-id', item.id);
+            }
+
+            fragment.append(sourceClone);
+        });
+
+        document.querySelector('.sources')?.append(fragment);
+    }
+}
+
+export default Sources;
