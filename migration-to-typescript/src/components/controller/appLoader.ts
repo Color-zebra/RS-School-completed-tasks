794b713c597edd0750c56211f192d96001e1483b
Romanenkov Dmitriy <215rd@mail.ru>
2023-06-10 17:58:49 +0300

    fix: delete useless rules from tsconfig

--- migration-to-typescript/src/components/controller/appLoader.ts
+++ migration-to-typescript/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://rss-news-api.onrender.com/' /* 'https://newsapi.org/v2/' */ /* 'http://127.0.0.1:8075/' */, {
+        super('https://rss-news-api.onrender.com/' /* 'http://127.0.0.1:8075/' */, {
             apiKey: '82717d1318ca471497c48dcbbe84a120',
         });
     }
