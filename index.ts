0683cfbd814ed447f0afd5ec75011a61cc5ee67d
Romanenkov Dmitriy <215rd@mail.ru>
2023-06-19 22:13:38 +0300

    feat: add and configure webpack

---
+++ index.ts
@@ -0,0 +1,5 @@
+import "./style.scss";
+import "./src/some";
+import "./index.html";
+
+console.log("Hello world");
---
+++ src/some.ts
@@ -0,0 +1,6 @@
+import img from "./assets/img/placeholder.png";
+
+console.log("I'm TS");
+console.log("I'm NOT TS");
+
+// document.body.style.background = `url(${img})`;
