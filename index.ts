32330cb71d3012afe485a1df5f451f0c081cfdac
Romanenkov Dmitriy <215rd@mail.ru>
2023-06-20 19:45:55 +0300

    feat: add base file structure

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import "./style.scss";
-import "./src/some";
 import "./index.html";
 
 console.log("Hello world");
--- src/some.ts
+++
@@ -1,6 +0,0 @@
-import img from "./assets/img/placeholder.png";
-
-console.log("I'm TS");
-console.log("I'm NOT TS");
-
-// document.body.style.background = `url(${img})`;
